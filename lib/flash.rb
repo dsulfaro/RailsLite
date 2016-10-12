180a3e90eb1b6cb7bb70f8ab512dbee2bf154bbf
Dan Sulfaro <dsulfaro1@gmail.com>
2016-10-11 17:57:11 -0700

    start flash; todo: finish flash.now

--- lib/flash.rb
+++ lib/flash.rb
@@ -1,4 +1,31 @@
 require 'json'
 
 class Flash
+
+  def initialize(req)
+    if req.cookies != {}
+      @cookies = JSON.parse(req.cookies["_rails_lite_app_flash"])
+    else
+      @cookies = {}
+    end
+  end
+
+  def []=(key, val)
+    @cookies[key] = val
+  end
+
+  def [](key)
+    @cookies[key.to_s]
+  end
+
+  def store_flash(res)
+    json = @cookies.to_json
+    cookies_attributes = { path: "/", value: json }
+    res.set_cookie("_rails_lite_app_flash", cookies_attributes)
+  end
+
+  def now
+    ## TODO ##
+  end
+
 end
