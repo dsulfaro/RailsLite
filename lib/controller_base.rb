c7fee840441f71d4cb8056e446e6cb67ad70abc5
Dan Sulfaro <dsulfaro1@gmail.com>
2016-10-11 17:07:09 -0700

    finish router

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -8,8 +8,9 @@ class ControllerBase
   attr_reader :req, :res, :params
 
   # Setup the controller
-  def initialize(req, res)
+  def initialize(req, res, route_params = {})
     @req, @res = req, res
+    @params = route_params.merge(req.params)
   end
 
   # Helper method to alias @already_built_response
@@ -33,7 +34,6 @@ class ControllerBase
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
-    byebug
     unless already_built_response?
       @res['Content-Type'] = content_type
       @res.write(content)
@@ -42,7 +42,7 @@ class ControllerBase
       # double render error -- return? raise?
       raise "Double render error"
     end
-    session.store_session(@req)
+    session.store_session(@res)
   end
 
   # use ERB and binding to evaluate templates
@@ -61,10 +61,12 @@ class ControllerBase
 
   # method exposing a `Session` object
   def session
-    @session ||= Session.new(@res)
+    @session ||= Session.new(@req)
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
+    self.send(name)
+    render(name) unless already_built_response?
   end
 end
